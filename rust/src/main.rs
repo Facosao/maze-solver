9a140463cde6000d3448ef9ea92e07a886066a50
Facosao <leonardosalgado02@hotmail.com>
2023-11-05 13:15:31 -0300

    Rust: Correção para ler argv

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -8,40 +8,37 @@ fn main() {
     let mut address: Option<String> = None;
     let mut maze: Option<String> = None;
 
-    println!("maze-solver (Rust)");
+    let args: Vec<_> = std::env::args().collect();
+    let mut i = 0;
 
-    let mut iter = std::env::args();
-
-    match iter.next() {
-        Some(arg) => {
-            match arg.as_str() {
-                "--url" => {
-                    address = Some(iter.next().unwrap());
-                }
-
-                "--maze" => {
-                    maze = Some(iter.next().unwrap());
-                }
-
-                "--help" => {
-                    println!("Usage: main.py [OPTION] [ARG]\n");
-                    println!(" [OPTION]  [OPTION ARG]");
-                    println!(" --url     Specify a custom URL for the API Calls.");
-                    println!("           Default URL: https://gtm.delary.dev");
-                    println!("");
-                    println!(" --maze    Specify a custom maze for the program.");
-                    println!("           Default maze: maze-sample");
-                    println!("");
-                    println!(" --help    Displays this help text. (No args)");
-                    println!("");
-                    return;
-                }
-
-                &_ => {}
+    while i < args.len() {
+        match args[i].as_str() {
+            "--url" => {
+                address = Some(args[i + 1].clone());
             }
+
+            "--maze" => {
+                maze = Some(args[i + 1].clone());
+            }
+
+            "--help" => {
+                println!("Usage: main.py [OPTION] [ARG]\n");
+                println!(" [OPTION]  [OPTION ARG]");
+                println!(" --url     Specify a custom URL for the API Calls.");
+                println!("           Default URL: https://gtm.delary.dev");
+                println!("");
+                println!(" --maze    Specify a custom maze for the program.");
+                println!("           Default maze: maze-sample");
+                println!("");
+                println!(" --help    Displays this help text. (No args)");
+                println!("");
+                return;
+            }
+
+            &_ => {}
         }
-        
-        None => {}
+
+        i += 1;
     }
 
     solver::solver(address, maze);
