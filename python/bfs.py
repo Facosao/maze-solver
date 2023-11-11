dd178ce591d24e36e2fb88af9bb9666cd63186df
Facosao <leonardosalgado02@hotmail.com>
2023-11-11 15:33:26 -0300

    FINALMENTE! Protótipo do BFS duplo

--- python/bfs.py
+++ python/bfs.py
@@ -51,6 +51,41 @@ q[15].adjacent = [q[7]]
 q[14].goal = True
 
 
+def BFS_light(nodes: list[Node], start: Node, end: int) -> list[int]:
+    previous: list[int] = [0 for _ in range(len(nodes) + 1)]
+    visited: list[int] = [0 for _ in range(len(nodes) + 1)]
+    queue: list[int] = []
+
+    visited[start.id] = 1
+    queue.append(start.id)
+
+    while len(queue) > 0:
+        v = nodes[queue.pop(0)]
+        if v.id == end:
+            # Generate path to return
+            path: list[int] = []
+            aux = v
+            while True:
+                if previous[aux.id] != 0:
+                    path.append(aux.id)
+                    aux = nodes[previous[aux.id]]
+                else:
+                    path.append(aux.id)
+                    break
+
+            path.reverse()
+            print("S:", start.id, "E:", end, "L:", path)
+            return path
+
+        for node in v.adjacent:
+            if visited[node.id] == 0:
+                visited[node.id] = 1
+                previous[node.id] = v.id
+                queue.append(node.id)
+
+    raise RuntimeError()
+
+
 def BFS(nodes: list[Node], root: Node):
     queue: list[int] = []
     root.explored = True
@@ -60,7 +95,6 @@ def BFS(nodes: list[Node], root: Node):
 
     while len(queue) > 0:
         v = nodes[queue.pop(0)]
-        # v = nodes[queue.pop()]
 
         if start is False:
             if current_node == v.id:
@@ -68,12 +102,17 @@ def BFS(nodes: list[Node], root: Node):
                 # Only save node info if not present
                 # -- Handled by API call
             else:
-                print("M", v.id)
+                if v not in nodes[current_node].adjacent:
+                    path_to_node = BFS_light(nodes, nodes[current_node], v.id)
+                    path_to_node.extend(queue)
+                    queue = path_to_node
+                    continue
+                else:
+                    print("M", v.id)
         else:
             print("START", v.id)
 
         current_node = v.id
-        has_child = False
 
         if v.goal is True:
             return
@@ -82,23 +121,7 @@ def BFS(nodes: list[Node], root: Node):
             if node.explored is False:
                 node.previous = v
                 node.explored = True
-                # queue.extend(node.find_path())
-
-                if has_child is False:
-                    queue.extend(node.previous.find_path())
-                    has_child = True
-
                 queue.append(node.id)
-                queue.append(v.id)
-
-        if has_child is True:
-            # walk_back = v.find_path()
-            # walk_back.reverse()
-            # queue.extend(walk_back)
-            if v.previous is not None:
-                queue.append(v.previous.id)
-        else:
-            queue.append(v.id)
 
         start = False
 
